d9623964dd3867ccb184ccd3f59eeb004ab14cae
Wayne <61163734+1983Wayne@users.noreply.github.com>
2021-04-25 13:39:12 -0600

    Update heads_or_tails.py

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -7,14 +7,17 @@ import random
 def three_flips(amount):
     "Function that simulates coin-tosses for 'amount' of times."
     total_flips = 0
+    
     for i in range(amount):
         tails = False
         heads = False
         times = 0
         flips = 0
+        
         while times < 3:
             coin_toss = random.randint(0, 1)
             flips += 1
+            
             if coin_toss == 0 and tails == True:
                 times += 1
             elif coin_toss == 0 and tails == False:
@@ -27,10 +30,12 @@ def three_flips(amount):
                 times = 1
                 heads = True
                 tails = False
+                
             if coin_toss == 0:
                 print('H', end=' ')
             else:
                 print('T', end=' ')
+                
         print('(', flips, 'flips )')
         total_flips += flips
     print('On average,', (total_flips / amount), 'flips were needed.')
