5b3fbc112a5fd950164e474ec42aa44b29add589
Wayne <61163734+1983Wayne@users.noreply.github.com>
2021-05-06 18:17:05 -0600

    Update and rename anagrams_w_dictionary to anagrams_w_dictionary.py

--- anagrams_w_dictionary.py
+++ anagrams_w_dictionary.py
@@ -4,16 +4,22 @@ def check_if_anagram(string_1, string_2):
     legal_chars = 'abcdefghijklmnopqrstuvwxyz'
     string_1_dict = dict()
     string_2_dict = dict()
+    
     for i in string_1.lower().replace(' ', ''):
+        
         if i not in string_1_dict and i in legal_chars:
             string_1_dict[i] = 1
         else:
             string_1_dict[i] += 1
+            
     for i in string_2.lower().replace(' ', ''):
+        
         if i not in string_2_dict and i in legal_chars:
             string_2_dict[i] = 1
         else:
             string_2_dict[i] += 1
+            
     if string_1_dict != string_2_dict:
             return 'These are not anagrams.'
+        
     return 'These are anagrams.'
